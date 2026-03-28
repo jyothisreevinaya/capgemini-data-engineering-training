dc1b4563017fe94a86e39cfe22e98d8b89ecc250
jyothisreevinaya <jyothisreevinaya@gmail.com>
2026-03-28 14:13:33 +0530

    Update pyspark_code.py

--- week0/phase1/pyspark_code.py
+++ week0/phase1/pyspark_code.py
@@ -1,1 +1,12 @@
-print("pyspark");
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+customers = spark.createDataFrame([
+ (1, "Ravi", "Hyderabad", 25),
+ (2, "Sita", "Chennai", 32),
+ (3, "Arun", "Hyderabad", 28)
+], ["customer_id", "customer_name", "city", "age"])
+customers.show()
+customers.filter(customers.city == "Chennai").show()
+customers.filter(customers.age > 25).show()
+customers.select("customer_name", "city").show()
+customers.groupBy("city").count().show()
