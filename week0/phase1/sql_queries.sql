c7da9c40a54eb5c49efc5ac82a0556d10d60c71c
jyothisreevinaya <jyothisreevinaya@gmail.com>
2026-03-28 14:02:18 +0530

    Update sql_queries.sql

--- week0/phase1/sql_queries.sql
+++ week0/phase1/sql_queries.sql
@@ -1,1 +1,17 @@
-Select * from customers;
+# Schema SQL
+CREATE TABLE customers (
+ customer_id INT,
+ customer_name VARCHAR(50),
+ city VARCHAR(50),
+ age INT
+);
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', 25),
+(2, 'Sita', 'Chennai', 32),
+(3, 'Arun', 'Hyderabad', 28);
+# Queries SQL
+select * from customers;
+select * from customers where city = 'Chennai';
+select * from customers where age>25;
+select customer_name, city from customers;
+select city, count(*) from customers group by city;
